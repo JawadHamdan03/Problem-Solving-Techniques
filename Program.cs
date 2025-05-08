8aab372758b03ae48f9e0ef8e2f1db623bbe64ed
JawadHamdan03 <eng.jawad121@hotmail.com>
2025-05-08 18:44:50 +0300

    Brute Force

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Problem_Solving_Techniques;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BruteForce();
+    }
+
+
+    static void BruteForce()
+    {
+        Console.WriteLine("---------------------Brute Force------------------");
+        // Problem : search for target value in the array 
+        // Technique : try all possible solutions => try every thing , used for small inputs 
+        int[] numbers = { 1,3,5,7,9};
+        int target = 5;
+
+        foreach(int n in numbers)
+        {
+            if (n==target)
+            {
+                Console.WriteLine("target found \n");
+                return;
+            }
+        }
+        Console.WriteLine("target not found \n");
+    }
+
+
+
+
+
+}
